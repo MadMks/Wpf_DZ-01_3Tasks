e8f738f198e6a3ab12fdf6340a3059c6332f402f
MadMks <mks59k@gmail.com>
2018-05-19 22:42:34 +0300

    not work task 3

--- Task_3_RunningAwayButton/MainWindow.xaml.cs
+++ Task_3_RunningAwayButton/MainWindow.xaml.cs
@@ -23,6 +23,13 @@ namespace Task_3_RunningAwayButton
         public MainWindow()
         {
             InitializeComponent();
+
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            //this.btnRun.Lef
         }
     }
 }
