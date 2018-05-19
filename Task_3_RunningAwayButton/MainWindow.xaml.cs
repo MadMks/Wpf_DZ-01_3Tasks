3bcff96fba8f25cfcbd42996d812519ef40c65d8
MadMks <mks59k@gmail.com>
2018-05-20 01:16:15 +0300

    Not working task 3.

--- Task_3_RunningAwayButton/MainWindow.xaml.cs
+++ Task_3_RunningAwayButton/MainWindow.xaml.cs
@@ -20,6 +20,9 @@ namespace Task_3_RunningAwayButton
     /// </summary>
     public partial class MainWindow : Window
     {
+        private double clientRectWidth;
+        private double clientRectHeight;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -29,7 +32,41 @@ namespace Task_3_RunningAwayButton
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            //this.btnRun.Lef
+            Canvas.SetLeft(this.fieldAround, this.GetPointHorizontForCenteringButton());
+            Canvas.SetTop(this.fieldAround, this.GetPointVerticalForCenteringButton());
+        }
+
+        private double GetPointVerticalForCenteringButton()
+        {
+            return (this.canvas.ActualHeight / 2) - (this.fieldAround.ActualHeight / 2);
+        }
+
+        private double GetPointHorizontForCenteringButton()
+        {
+            return (this.canvas.ActualWidth / 2) - (this.fieldAround.ActualWidth / 2);
+        }
+
+        private void fieldAround_MouseEnter(object sender, MouseEventArgs e)
+        {
+            //Canvas.SetLeft(this.fieldAround, Canvas.GetLeft(this.fieldAround) + 1);
+        }
+
+        private void fieldAround_MouseMove(object sender, MouseEventArgs e)
+        {
+            // e.MouseDevice.GetPosition(sender as Canvas).
+
+            Rect rect = new Rect(
+                Canvas.GetLeft(this.fieldAround),
+                Canvas.GetTop(this.fieldAround),
+                this.fieldAround.ActualWidth,
+                this.fieldAround.ActualHeight
+                );
+            rect.Height = (sender as Canvas).ActualHeight;
+            rect.Width = (sender as Canvas).ActualWidth;
+            if (rect.Contains(e.MouseDevice.GetPosition(this.fieldAround)) == true)
+            {
+                Canvas.SetLeft(this.fieldAround, Canvas.GetLeft(this.fieldAround) + 10);
+            }
         }
     }
 }
